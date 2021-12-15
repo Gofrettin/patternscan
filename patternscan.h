066224a9964cbbed57265737427c8e8287451b3b
pyj2323 <32794121+pyj2323@users.noreply.github.com>
2021-12-15 11:35:00 +0900

    Update patternscan.h

--- patternscan.h
+++ patternscan.h
@@ -1,18 +1,12 @@
 #pragma once
 
-#ifdef PATTERNSCAN_USE64BIT
-typedef unsigned __int64  uintptr;
-#elif PATTERNSCAN_USE32BIT
-typedef unsigned int uintptr;
-#else
-typedef uintptr_t uintptr;
-#endif
-
 #include <Windows.h>
 #include <string>
 #include <functional>
+#include <type_traits>
 
-using TypeRPMFunc = std::function<bool(uintptr Address, void* Buffer, size_t Size)>;
+template<class Type>
+using TypeRPMFunc = std::function<bool(Type Address, void* Buffer, size_t Size)>;
 
 class PatternScan {
 private:
@@ -25,7 +19,7 @@ private:
 		return true;
 	}
 
-	static uintptr FindPattern(uintptr Address, size_t Len, uint8_t* bMask, uint8_t* vMask) {
+	static uintptr_t FindPattern(uintptr_t Address, size_t Len, uint8_t* bMask, uint8_t* vMask) {
 		for (size_t i = 0; i < Len; i++)
 			if (DataCompare((uint8_t*)(Address + i), bMask, vMask))
 				return Address + i;
@@ -35,7 +29,7 @@ private:
 	static bool ConvertStringToMask(std::string strPattern, uint8_t* bMask, uint8_t* vMask, size_t MaxSize) {
 		memset(bMask, 0, MaxSize);
 		memset(vMask, 0, MaxSize);
-		uintptr i = 0, j = 0, k = 0;
+		uintptr_t i = 0, j = 0, k = 0;
 		while (strPattern[i]) {
 			if (strPattern[i] == ' ') {
 				k = 0;
@@ -74,7 +68,10 @@ private:
 		return true;
 	}
 
-	static bool GetModuleInfo(uintptr ModuleBase, DWORD& BaseOfCode, DWORD& SizeOfCode, TypeRPMFunc RPM) {
+	template <class Type>
+	static bool GetModuleInfo(Type ModuleBase, DWORD& BaseOfCode, DWORD& SizeOfCode, TypeRPMFunc<Type> RPM) {
+		static_assert(std::is_same<Type, uint32_t>::value | std::is_same<Type, uint64_t>::value, "Type must 32bit or 64bit pointer type");
+
 		IMAGE_DOS_HEADER DosHeader;
 		if (!RPM(ModuleBase, &DosHeader, sizeof(DosHeader)))
 			return false;
@@ -82,7 +79,11 @@ private:
 		if (DosHeader.e_magic != IMAGE_DOS_SIGNATURE)
 			return false;
 
-		IMAGE_NT_HEADERS NtHeaders;
+		using NtHeadersType = IMAGE_NT_HEADERS64;
+		if constexpr (std::is_same<Type, uint32_t>::value)
+			NtHeadersType = IMAGE_NT_HEADERS32;
+		
+		NtHeadersType NtHeaders;
 		if (!RPM(ModuleBase + DosHeader.e_lfanew, &NtHeaders, sizeof(NtHeaders)))
 			return false;
 
@@ -95,12 +96,15 @@ private:
 	}
 
 public:
-	static uintptr Range(uintptr Address, size_t Len, uint8_t* bMask, uint8_t* vMask, TypeRPMFunc RPM) {
-		size_t SizeOfPattern = strlen((const char*)bMask) - 1;
-		uintptr Start = Address & ~0xFFF;
-		uintptr End = (Address + Len + 0x1000) & ~0xFFF;
+	template <class Type>
+	static Type Range(Type Address, size_t Len, uint8_t* bMask, uint8_t* vMask, TypeRPMFunc<Type> RPM) {
+		static_assert(std::is_same<Type, uint32_t>::value | std::is_same<Type, uint64_t>::value, "Type must 32bit or 64bit pointer type");
 
-		for (uintptr i = Start; i < End; i += 0x1000) {
+		size_t SizeOfPattern = strlen((const char*)bMask) - 1;
+		Type Start = Address & ~0xFFF;
+		Type End = (Address + Len + 0x1000) & ~0xFFF;
+
+		for (Type i = Start; i < End; i += 0x1000) {
 			uint8_t Buf[0x2000];
 
 			if (!RPM(i, Buf, 0x1000))
@@ -108,14 +112,14 @@ public:
 
 			RPM(i + 0x1000, Buf + 0x1000, SizeOfPattern);
 
-			uintptr AddressTemp = (uintptr)Buf;
+			Type AddressTemp = (Type)Buf;
 			while (1) {
-				uintptr LocalResult = FindPattern(AddressTemp, 0x1000, bMask, vMask);
+				Type LocalResult = FindPattern(AddressTemp, 0x1000, bMask, vMask);
 				if (LocalResult == 0)
 					break;
 				else {
-					uintptr Result = LocalResult - (uintptr)Buf + i;
-					if (Result >= Address && Result < Address + Len && Result != (uintptr)vMask)
+					Type Result = LocalResult - (Type)Buf + i;
+					if (Result >= Address && Result < Address + Len && Result != (Type)vMask)
 						return Result;
 
 					AddressTemp = LocalResult + 1;
@@ -125,7 +129,10 @@ public:
 		return 0;
 	}
 
-	static uintptr Range(uintptr Address, size_t Len, std::string strPattern, TypeRPMFunc RPM) {
+	template <class Type>
+	static Type Range(Type Address, size_t Len, std::string strPattern, TypeRPMFunc<Type> RPM) {
+		static_assert(std::is_same<Type, uint32_t>::value | std::is_same<Type, uint64_t>::value, "Type must 32bit or 64bit pointer type");
+
 		uint8_t bMask[MAX_MASK_SIZE] = { 0 };
 		uint8_t vMask[MAX_MASK_SIZE] = { 0 };
 		if (!ConvertStringToMask(strPattern, bMask, vMask, MAX_MASK_SIZE))
@@ -134,19 +141,41 @@ public:
 		return Range(Address, Len, bMask, vMask, RPM);
 	}
 
-	static uintptr Module(uintptr ModuleBase, uint8_t* bMask, uint8_t* vMask, TypeRPMFunc RPM) {
+	template <class Type>
+	static Type Module(Type ModuleBase, uint8_t* bMask, uint8_t* vMask, TypeRPMFunc<Type> RPM) {
+		static_assert(std::is_same<Type, uint32_t>::value | std::is_same<Type, uint64_t>::value, "Type must 32bit or 64bit pointer type");
+
 		DWORD BaseOfCode, SizeOfCode;
-		if (!GetModuleInfo(ModuleBase, BaseOfCode, SizeOfCode, RPM))
+		if (!GetModuleInfo<Type>(ModuleBase, BaseOfCode, SizeOfCode, RPM))
 			return 0;
 
 		return Range(ModuleBase + BaseOfCode, SizeOfCode, bMask, vMask, RPM);
 	}
 
-	static uintptr Module(uintptr ModuleBase, std::string strPattern, TypeRPMFunc RPM) {
+	template <class Type>
+	static Type Module(Type ModuleBase, std::string strPattern, TypeRPMFunc<Type> RPM) {
+		static_assert(std::is_same<Type, uint32_t>::value | std::is_same<Type, uint64_t>::value, "Type must 32bit or 64bit pointer type");
+
 		DWORD BaseOfCode, SizeOfCode;
-		if (!GetModuleInfo(ModuleBase, BaseOfCode, SizeOfCode, RPM))
+		if (!GetModuleInfo<Type>(ModuleBase, BaseOfCode, SizeOfCode, RPM))
 			return 0;
 
 		return Range(ModuleBase + BaseOfCode, SizeOfCode, strPattern, RPM);
+	}
+
+	static uintptr_t Range(uintptr_t Address, size_t Len, uint8_t* bMask, uint8_t* vMask, TypeRPMFunc<uintptr_t> RPM) {
+		return Range<uintptr_t>(Address, Len, bMask, vMask, RPM);
+	}
+
+	static uintptr_t Range(uintptr_t Address, size_t Len, std::string strPattern, TypeRPMFunc<uintptr_t> RPM) {
+		return Range<uintptr_t>(Address, Len, strPattern, RPM);
+	}
+
+	static uintptr_t Module(uintptr_t ModuleBase, uint8_t* bMask, uint8_t* vMask, TypeRPMFunc<uintptr_t> RPM) {
+		return Module<uintptr_t>(ModuleBase, bMask, vMask, RPM);
+	}
+
+	static uintptr_t Module(uintptr_t ModuleBase, std::string strPattern, TypeRPMFunc<uintptr_t> RPM) {
+		return Module<uintptr_t>(ModuleBase, strPattern, RPM);
 	}
 };
